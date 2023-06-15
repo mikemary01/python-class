3d66e6654845de9afee90b1ed7faf31562c37526
EC2 Default User <ec2-user@ip-10-16-10-172.us-west-2.compute.internal>
2023-06-15 15:24:55 +0000

    initial commit

---
+++ File1.py
@@ -0,0 +1,11 @@
+print("Python has three numeric types: int, float, and complex")
+
+# myValue=15j
+# print(myValue)
+# print(type(myValue))
+# print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue="15j" + "goal"
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
---
+++ string-data-type.py
@@ -0,0 +1,23 @@
+myString = "This is a string."
+print(myString)
+print(type(myString))
+print(myString + " is of the data type " + str(type(myString)))
+
+firststring ="Water"
+secondstring ="Fall"
+thirdstring =firststring + secondstring
+print(thirdstring)
+
+firststring ="Ogbu"
+secondstring =" Michael"
+thirdstring =" Iji"
+fourthstring =firststring + secondstring + thirdstring
+print(fourthstring)
+
+name = input("What is your name? ")
+print(name)
+
+color = input("What is your favorite color?, ")
+animal = input("What is your favorite animal?, ")
+print("{}, you like a {} {}!".format(name,color,animal))
+
